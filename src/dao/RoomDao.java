ebc5a05304bbacfb204dd0bd08d63125733d07f2
yakovalenko <jaroslawkovalenko@jmail.com>
2016-11-08 16:07:47 +0200

    roomDao

---
+++ src/dao/RoomDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import entities.Room;
+
+import java.util.List;
+
+public class RoomDao implements Dao<Room>{
+    @Override
+    public void add(Room room) {
+        
+    }
+
+    @Override
+    public void update(Room room) {
+
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public Room findByID(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Room> getAll() {
+        return null;
+    }
+}
