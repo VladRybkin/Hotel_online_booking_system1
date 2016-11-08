e34c1501af0db4369f6ce18af6048ece0dec41a5
Димон <1983zubarev@gmail.com>
2016-11-08 17:25:24 +0200

    Hotel & Room

--- src/entities/Room.java
+++ src/entities/Room.java
@@ -2,6 +2,7 @@ package entities;
 
 import src.enums.RoomType;
 
+import java.util.Currency;
 import java.util.Random;
 
 public class Room {
@@ -9,24 +10,27 @@ public class Room {
     private long id;
     private int roomNumber;
     private int price;
+    private Currency currency;
     private int persons;
     private RoomType roomType;
     private User reservedForUser;
 
     //TODO поле Currency перенести из сущности Рум в сущность Хотел
     //поле hotel убрать, будем доставать обратно зная отель и перебирая его комнаты
-    public Room(int number, int price, int persons, RoomType roomType) {
+    public Room(int number, int price, Currency currency, int persons, RoomType roomType) {
 //        this.id = TextUtils.getLastId("Room"); TextUtils пока досвечивается красным
         this.roomNumber = number;
         this.price = price;
+        this.currency = currency;
         this.persons = persons;
         this.roomType = roomType;
     }
 
-    public Room(long id, int roomNumber, int price, int persons, RoomType roomType) {
+    public Room(long id, int roomNumber, int price, Currency currency, int persons, RoomType roomType) {
         this.id = id;
         this.roomNumber = roomNumber;
         this.price = price;
+        this.currency = currency;
         this.persons = persons;
         this.roomType = roomType;
     }
@@ -62,6 +66,8 @@ public class Room {
         this.reservedForUser = reservedForUser;
     }
 
+    public Currency getCurrency() {return currency;}
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -73,6 +79,7 @@ public class Room {
         if (roomNumber != room.roomNumber) return false;
         if (price != room.price) return false;
         if (persons != room.persons) return false;
+        if (!currency.equals(room.currency)) return false;
         if (roomType != room.roomType) return false;
         return reservedForUser.equals(room.reservedForUser);
     }
@@ -82,11 +89,14 @@ public class Room {
         int result = (int) (id ^ (id >>> 32));
         result = 31 * result + roomNumber;
         result = 31 * result + price;
+        result = 31 * result + currency.hashCode();
         result = 31 * result + persons;
-        result = 31 * result + (roomType != null ? roomType.hashCode() : 0);
+        result = 31 * result + roomType.hashCode();
+        result = 31 * result + reservedForUser.hashCode();
         return result;
     }
-//    Под старое поле
+
+    //    Под старое поле
 //    @Override
 //    public String toString() {
 //        return  "" + hotel + ", " +"room " + "№" + roomNumber +
