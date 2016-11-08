2fbc847bac4190afd7ca3bf1624652a1c3f40bfe
Oleg Small <smolovik.oleg@gmail.com>
2016-11-08 16:19:01 +0200

    added method add in roomDao

--- src/dao/RoomDao.java
+++ src/dao/RoomDao.java
@@ -1,13 +1,23 @@
 package dao;
 
 import entities.Room;
+import util.TextUtil;
 
 import java.util.List;
 
 public class RoomDao implements Dao<Room>{
     @Override
     public void add(Room room) {
-        
+        StringBuffer stringRoom = new StringBuffer();
+
+        stringRoom.append(room.getId()).append(TextUtil.getSeparator());
+        stringRoom.append(room.getRoomNumber()).append(TextUtil.getSeparator());
+        stringRoom.append(room.getPersons()).append(TextUtil.getSeparator());
+        stringRoom.append(room.getRoomType().name()).append(TextUtil.getSeparator());
+        stringRoom.append(room.getPrice()).append(TextUtil.getSeparator());
+        stringRoom.append(room.getReservedForUser().getId());
+
+        TextUtil.writeToFile(TextUtil.getRoomFileName(), stringRoom.toString());
     }
 
     @Override
